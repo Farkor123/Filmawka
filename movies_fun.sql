198530bd666f38aab7a619e31c62476d0cf31004
Jakub Łokieć <jakublokiec@wp.pl>
2019-03-29 15:11:44 +0100

    Create get_movies_for_actor procedure
    The procedure returns a formatted string with all movies in which actor
    with given actor_id starred.

--- movies_fun.sql
+++ movies_fun.sql
@@ -1,5 +1,38 @@
 use filmawka;
 
+drop procedure if exists get_movies_for_actor;
+DELIMITER //
+create procedure get_movies_for_actor(in actor_id int, inout output text)
+begin
+	declare title varchar(50);
+    declare release_date date;
+    declare finished boolean default false;
+    
+    declare movie_cursor cursor for
+    select m.title, m.release_date
+    from actor_movie am
+    join movies m on am.movie_id = m.movie_id
+    where am.actor_id = actor_id
+    order by m.release_date desc;
+    
+    declare continue handler for not found set finished = true;
+    
+    open movie_cursor;
+    get_movies: loop
+    
+    fetch movie_cursor into title, release_date;
+    
+    if finished = true then
+		leave get_movies;
+	end if;
+    
+    set output = concat(output, extract(year from release_date), ' ', title, '\n');
+    
+    end loop get_movies;
+    close movie_cursor;
+end//
+DELIMITER ;
+
 drop procedure if exists actor_info;
 DELIMITER //
 create procedure actor_info(in actor_id int)
