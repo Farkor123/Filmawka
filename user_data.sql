22c4f3043decce1d46876e0c52fd1e8b1117c5ac
pixrl <blackpixel@protonmail.com>
2019-03-31 10:46:58 +0200

    Added conversation replies to user_data.sql

--- user_data.sql
+++ user_data.sql
@@ -130,7 +130,61 @@ insert into account_settings (account_settings_id, user_id, night_mode, timezone
 insert into account_settings (account_settings_id, user_id, night_mode, timezone, is_newsletter) values (42, 42, 1, 'America/New_York', 1);
 
 insert into conversation(conversation_id, user_one, user_two, time) values (1, 13, 11, 1366513972);
+insert into conversation(conversation_id, user_one, user_two, time) values (2, 3, 4, 1366623972);
+insert into conversation(conversation_id, user_one, user_two, time) values (3, 14, 17, 1366534356);
+insert into conversation(conversation_id, user_one, user_two, time) values (4, 1, 2, 1366513972);
+insert into conversation(conversation_id, user_one, user_two, time) values (5, 20, 23, 1367622111);
+insert into conversation(conversation_id, user_one, user_two, time) values (6, 40, 41, 1366523000);
+insert into conversation(conversation_id, user_one, user_two, time) values (7, 33, 35, 1366513972);
+insert into conversation(conversation_id, user_one, user_two, time) values (8, 16, 17, 1366712352);
+insert into conversation(conversation_id, user_one, user_two, time) values (9, 12, 19, 1366854572);
+insert into conversation(conversation_id, user_one, user_two, time) values (10, 25, 27, 1366883972);
 
-insert into conversation_reply(1, 'Cześć', 11, 1366513972, 1);
-insert into conversation_reply(2, 'Cześć Millard. Chcesz iść ze mną na film?', 13, 1366514567, 1);
-insert into conversation_reply(3, 'Chętnie', 11, 1366515467, 1);
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (1, 'Cześć', 11, 1366513972, 1);
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (2, 'Cześć Millard. Chcesz iść ze mną na film?', 13, 1366514567, 1);
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (3, 'Chętnie', 11, 1366515467, 1);
+
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (4, 'Hej, oglądałeś batmana?', 4, 1366623972, 2);
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (5, 'Nie', 3, 1366623999, 2);
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (6, 'Bardzo fajny film, polecam', 4, 1366624972, 2);
+
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (7, 'Lubisz filmy Stanleya Kubricka?', 17, 1366534356, 3);
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (8, 'Tak, oglądałem Lśnienie i bardzo mi się podobało', 14, 1366624572, 3);
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (9, 'Ja widziałem Odyseje Kosmiczną i też mi się bardzo podobało', 17, 1366625231, 3);
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (10, 'To fajnie', 14, 1366627865, 3);
+
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (11, 'Hej jaki serial polecasz?', 2, 1366516512, 4);
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (12, 'Polecam serial pod tytułem "Młody Papież".', 1, 1366536691, 4);
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (13, 'Dzięki.', 2, 1366539201, 4);
+
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (14, 'Hej', 23, 1367622111, 5);
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (15, 'Hej', 20, 1367622222, 5);
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (16, 'Co tam?', 23, 1367625467, 5);
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (17, 'Dobrze, a tam?', 20, 1367626565, 5);
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (18, 'W porządku', 23, 1367670000, 5);
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (19, 'To dobrze', 20, 1367671234, 5);
+
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (20, 'Witam', 41, 1366523000, 6);
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (21, 'Witam', 40, 1366524444, 6);
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (22, 'Dobranoc', 41, 1366526000, 6);
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (23, 'Dobranoc', 40, 1366527132, 6);
+
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (24, 'Hej', 35, 1366526564, 7);
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (25, 'Hej', 33, 1366535401, 7);
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (26, 'Hej', 35, 1366536011, 7);
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (27, 'Hej', 33, 1366537654, 7);
+
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (28, 'Hej', 17, 1366712352, 8);
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (29, 'Hej', 16, 1366742352, 8);
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (30, 'Hej', 17, 1366762352, 8);
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (31, 'Hej', 16, 1366772352, 8);
+
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (32, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ', 19, 1366854572, 9);
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (33, 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 12, 1366864572, 9);
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (34, 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', 19, 1366874572, 9);
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (35, 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 12, 1366884572, 9);
+
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (36, 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.', 27, 1366883972, 10);
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (37, 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. ', 25, 1366884972, 10);
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (38, 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?', 27, 1366885972, 10);
+insert into conversation_reply(conversation_reply_id, reply, user_id, time, conversation_id) values (39, ' Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?', 25, 1366886972, 10);
