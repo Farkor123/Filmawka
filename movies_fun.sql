48f76cdd7e2390f8ade1b03ed318b89536c5df8b
Jakub Łokieć <jakublokiec@wp.pl>
2019-04-28 16:38:51 +0200

    Create procedure for updating director summary

--- movies_fun.sql
+++ movies_fun.sql
@@ -564,6 +564,20 @@ begin
 end//
 DELIMITER ;
 
+drop procedure if exists update_director_summary;
+DELIMITER //
+create procedure update_director_summary(in full_name varchar(61), in new_summary text)
+begin
+	declare director_id int;
+    
+    select get_director_id_for_full_name(full_name) into director_id;
+    
+    update directors d
+    set d.summary = new_summary
+    where d.director_id = director_id;
+end//
+DELIMITER ;
+
 drop procedure if exists show_popular_movies_ranking;
 DELIMITER //
 create procedure show_popular_movies_ranking(in category_name varchar(30))
