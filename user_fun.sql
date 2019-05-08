aca22a87d04f88b596af8d407ad21514a75ed30a
pixrl <blackpixel@protonmail.com>
2019-05-08 22:35:41 +0200

    Added functions get_user_email, get_user_personal_data, get_user_timezone, get_user_conversation_reply

--- user_fun.sql
+++ user_fun.sql
@@ -25,3 +25,43 @@ begin
     return username;
 end//
 DELIMITER ;
+
+drop function if exists get_user_email;
+DELIMITER //
+create function get_user_email(user_id int) returns varchar(50) reads sql data
+begin
+	declare email varchar(50);
+    select u.`email` from users u where u.user_id = user_id into email;
+    return email;
+end//
+DELIMITER ;
+
+drop function if exists get_user_personal_data;
+DELIMITER //
+create function get_user_personal_data(user_id int) returns varchar(50) reads sql data
+begin
+	declare person_data varchar(40);
+    select CONCAT(u.`first_name`, ' ', u.`last_name`) from users u where u.user_id = user_id into person_data;
+    return person_data;
+end//
+DELIMITER ;
+
+drop function if exists get_user_timezone;
+DELIMITER //
+create function get_user_timezone(user_id int) returns varchar(30) reads sql data
+begin
+	declare user_timezone varchar(30);
+    select a.`timezone` from account_settings a where a.user_id = user_id into user_timezone;
+    return user_timezone;
+end//
+DELIMITER ;
+
+drop function if exists get_user_conversation_reply;
+DELIMITER //
+create function get_user_conversation_reply(conversation_reply_id int) returns text reads sql data
+begin
+	declare reply varchar(30);
+    select c.`reply` from conversation_reply c where c.conversation_reply_id = conversation_reply_id into reply;
+    return reply;
+end//
+DELIMITER ;
